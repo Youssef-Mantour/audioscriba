8b3627eeee72a182737025fd61e7d97027cf0f1c
youssef mantour <youssefmantour@gmail.com>
2025-03-15 10:43:41 +0000

    last change

--- src/app/audio/VoiceSelector.js
+++ src/app/audio/VoiceSelector.js
@@ -7,14 +7,14 @@ export default function VoiceSelector({ selectedVoice, handleVoiceChange }) {
   return (
     <Grid container spacing={2} justifyContent="center" sx={{ mb: 3 }}>
       {voices.map((voice) => (
-        <Grid item key={voice} sx={{ textAlign: "center", cursor: "pointer" }} onClick={() => handleVoiceChange(voice)}>
-          <Image src={`/avatars-voice/${voice}.jpg`} alt={voice} width={50} height={50} optimized="true" style={{
+        <Grid item key={voice} sx={{ mb: 0,textAlign: "center", cursor: "pointer" }} onClick={() => handleVoiceChange(voice)}>
+          <Image src={`/avatars-voice/${voice}.jpg`} alt={voice} width={40} height={40} optimized="true" style={{
             borderRadius: "50%",
             border: selectedVoice === voice ? "3px solid #009688" : "none",
             boxShadow: selectedVoice === voice ? "0px 2px 10px #009688" : "0px 2px 10px #616161",
             transition: "all 0.3s ease-in-out",
           }}/>
-          <Typography variant="body2" sx={{ mt: 0 }}>{voice}</Typography>
+          <Typography variant="body1" sx={{ mt: 0 }}>{voice}</Typography>
         </Grid>
       ))}
     </Grid>
