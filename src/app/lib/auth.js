bdfa338573b71dc23788ff744d62bf529f4c8d95
youssef mantour <youssefmantour@gmail.com>
2025-07-20 11:54:21 +0100

    fo

--- src/app/lib/auth.js
+++
@@ -1,6 +0,0 @@
-import { getServerSession } from "next-auth/next";
-import { authOptions } from "@/api/auth/[...nextauth]";
-
-export const getSession = async () => {
-  return await getServerSession(authOptions);
-};
