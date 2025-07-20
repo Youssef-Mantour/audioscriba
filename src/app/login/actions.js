3a88a8df19ecfeac1bf1dd2e4d772452425432bc
youssef mantour <youssefmantour@gmail.com>
2025-07-20 19:49:17 +0100

    Update actions.js
    g

--- src/app/login/actions.js
+++ src/app/login/actions.js
@@ -53,7 +53,7 @@ export async function signInWithGoogle() {
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider: 'google',
     options: {
-      redirectTo: 'http://localhost:3000/auth/callback?next=/languages-board',
+      redirectTo: `${process.env.NEXTAUTH_URL}/auth/callback?next=/languages-board`,
     },
   });
 
