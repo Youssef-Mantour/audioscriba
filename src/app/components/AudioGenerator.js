42a5b584429d767b70c8a479591022a7353a16b0
youssef mantour <youssefmantour@gmail.com>
2025-08-05 19:31:26 +0100

    ss

--- src/app/components/AudioGenerator.js
+++ src/app/components/AudioGenerator.js
@@ -193,7 +193,7 @@ export default function AudioGenerator({ language, voices }) {
   };
 
   return (
-    <Box sx={{ display: 'flex', minHeight: '100vh', width: '100%' }}>
+    <Box sx={{ display: 'flex', minHeight: '100vh', width: '100%' , mt:10 }}>
       {/* Sidebar */}
       <Box
         sx={{
