5bca27d5875ceb9a9597bb558ddbedae618961f5
youssef mantour <youssefmantour@gmail.com>
2025-08-08 02:55:40 +0100

    s

--- src/app/components/FormatSelector.js
+++ src/app/components/FormatSelector.js
@@ -1,16 +1,30 @@
 // FormatSelector.js
-import { RadioGroup, FormControl, FormLabel, Radio, FormControlLabel } from "@mui/material";
+import {
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  Typography,
+} from "@mui/material";
 
 export const formats = ["mp3", "opus", "aac", "flac", "pcm", "wav"];
+
 export default function FormatSelector({ responseFormat, handleFormatChange }) {
   return (
-    <FormControl sx={{ mb: 3 }}>
-      
-      <RadioGroup value={responseFormat} onChange={handleFormatChange} row>
+    <FormControl fullWidth sx={{ mb: 3 }}>
+      <InputLabel id="format-selector-label">Select Format</InputLabel>
+      <Select
+        labelId="format-selector-label"
+        value={responseFormat}
+        label="Select Format"
+        onChange={(e) => handleFormatChange(e.target.value)}
+      >
         {formats.map((format) => (
-          <FormControlLabel key={format} value={format} control={<Radio />} label={format.toUpperCase()} />
+          <MenuItem key={format} value={format}>
+            <Typography variant="body2">{format.toUpperCase()}</Typography>
+          </MenuItem>
         ))}
-      </RadioGroup>
+      </Select>
     </FormControl>
   );
 }
